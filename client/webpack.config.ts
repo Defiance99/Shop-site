f1ed6ae5db5dc31d04f7e07f973cfe4df2599318
Vladimir <var.alo@mail.ru>
2020-09-23 19:05:00 +0300

    added webpack build

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -1,7 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
